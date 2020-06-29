dd4a3f99ca94d97f3af8a6fa46edd211b12e755b
fantamady <fms294@nyu.edu>
2020-06-28 23:43:41 -0400

    Component part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';
+
 
 class App extends Component{
   render(){
@@ -9,6 +11,7 @@ class App extends Component{
         <Navbar dark color="primary">
           <div className="container">
               <NavbarBrand href="/">Nucamp</NavbarBrand>
+              <Directory/>
           </div>
         </Navbar>
       </div>
@@ -17,4 +20,4 @@ class App extends Component{
   }
 }
 
-export default App;
+ export default  App;
